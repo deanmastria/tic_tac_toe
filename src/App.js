342078df17e3ab47e2fc5cac471b2e76458af0ea
Dean Mastria <mastriadata@gmail.com>
2024-07-08 16:46:57 -0400

    moves history working

--- src/App.js
+++ src/App.js
@@ -20,8 +20,7 @@ function Board({ xIsNext, squares, onPlay }) {
     } else {
       nextSquares[i] = 'O';
     }
-    onplay(nextSquares);
-  
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -63,6 +62,23 @@ export default function Game() {
       setHistory([...history, nextSquares]);
       setXIsNext(!xIsNext);
   }
+  function jumpTo(nextMove) {
+// osdihfosdhf
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className="game">
@@ -70,7 +86,7 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-          <ol>{/*todo*/}</ol>
+          <ol>{moves}</ol>
       </div>
     </div>
   );
